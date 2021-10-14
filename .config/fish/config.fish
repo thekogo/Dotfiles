83db3ecfa9e4be8cd21283da9bc9dd463b389cf5
thekogo <thekogo@gmail.com>
2021-10-14 07:58:37 +0700

    Add fish shell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,65 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+alias dps "docker ps"
+alias dc "docker-compose"
+alias gowork "cd ~/go/src/github.com/"
+
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# Docker
+function da
+  docker exec -it $argv /bin/bash
+end
+
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -x GOPATH $HOME/go
+set -gx PATH $PATH $GOPATH/bin
+
+# NVM
+function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+  status --is-command-substitution; and return
+
+  if test -f .nvmrc; and test -r .nvmrc;
+    nvm use
+  else
+  end
+end
+
+switch (uname)
+  case Darwin
+    source (dirname (status --current-filename))/config-osx.fish
+  case Linux
+    # Do nothing
+  case '*'
+    source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+  source $LOCAL_CONFIG
+end
